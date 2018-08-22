407ff9392ad4e69adb27907f72b1d512730a2cdc
leolleocomp <leolleo.comp@gmail.com>
2018-08-22 20:28:25 -0300

    Add - suppor for es6 + object-rest-spread

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import _ from 'lodash';
 
 function updateJavascript() {
 	let element = document.getElementById('javascript');
+	const { name, ...rest } = { name: 'José', lastName: 'Penha'};
+
+	console.log(name, rest);
 
 	element.innerHTML = _.join(['ECMASCRIPT' , ' 6!'], ' ');
 }
